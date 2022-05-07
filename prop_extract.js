cdb8877243e0f2752c44b33fa5547bfc3af5ab73
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-05-07 11:09:22 +0200

    refactor: improved the code readability

--- prop_extract.js
+++ prop_extract.js
@@ -14,52 +14,66 @@
 // ]
 
 
-// function to extract valid properties from parseStrings, will return an array of objects
-// with property names as keys and prop values as values
+/*
+    Function to extract valid properties from parseStrings, will return an array of objects
+    with property names as keys and prop values as values
+*/
 function extractParseProp(parseStrings) {
+
     const regex = /(?<key>[a-zA-Z]+)=["|'](?<val>[^"']*)['|"]/gm
-    propertiesArr = []
+    const propertiesArr = [];
+
     // TODO: the following part is incomplete
     parseStrings.forEach(str => {
-        start = str.indexOf("<parse ") + "<parse ".length
-        end = str.indexOf(">")
-        cleanStr = str.substring(start, end)
+
+        const start = str.indexOf("<parse ") + "<parse ".length;
+        const end = str.indexOf(">");
+
+        let cleanStr = str.substring(start, end);
+
         if (cleanStr === "") {
-            cleanStr = null
-            propertiesArr.push(cleanStr)
+            cleanStr = null;
+            propertiesArr.push(cleanStr);
         } else {
             // match = cleanStr.match(regex)
-            let resObj = {}
-            let match = regex.exec(cleanStr)
+            const resObj = {};
+            let match = regex.exec(cleanStr);
+
             do {
                 // console.log(`Match ${match.groups.key} ${match.groups.val}`);
                 resObj[match.groups.key] = (match.groups.val !== "") ? match.groups.val : null
             } while ((match = regex.exec(cleanStr)) !== null)
-            propertiesArr.push(resObj)
-        }
 
-    })
-    return propertiesArr
+            propertiesArr.push(resObj);
+        }
+    });
+
+    return propertiesArr;
 }
+
 // console.log(`Input: \n`, parseStrings)
 // props = extractParseProp(parseStrings)
 // console.log(`Output: \n`, props)
 
 
-// this function is used to generate the results array
+// This function is used to generate the results array
 function resultMaker(objects, strings, intervals) {
-    let results = []
+
+    const results = [];
+
     // TODO: check if length of the 3 arrays is equal, otherwise something is missing
-    for (i = 0; i < strings.length; i++) {
+    for (let i = 0; i < strings.length; i++) {
         results.push({
             "raw": strings[i],
             "properties": (objects[i] !== null) ? [objects[i]] : [],
             "from": intervals[i][0],
             "to": intervals[i][1],
-        })
+        });
     }
-    return results
+
+    return results;
 }
+
 
 module.exports = {
     extractParseProp,
