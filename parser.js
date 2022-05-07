0aadb9b8242ab502af476579e0149997e76b4dd0
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-05-07 12:08:18 +0200

    refactor: improved the code readability

--- parser.js
+++ parser.js
@@ -1,119 +1,182 @@
+
 const fs = require('fs');
-const {
-    extractParseProp,
-    resultMaker
-} = require("./prop_extract");
+const propExtract = require("./prop_extract");
+
+// TODO: fix variables declaration into correct scope (into the function or outside)
 
 // Array of invalid tags where inside parse tags will not be valid
-const notAllowed = ['<script', '<style', '<!-- '] // TODO: fix not allowed interval even if comment tag <!-- is followed by \n character
-const notAllowedEnd = ['</script>', '</style>', '-->']
-//opening and closing tag that we have to match
-const matchTag = "<parse"
-const matchEndTag = "</parse>"
+const notAllowed = ['<script', '<style', '<!-- '];
+// TODO: fix not allowed interval even if comment tag <!-- is followed by \n character
+
+const notAllowedEnd = ['</script>', '</style>', '-->'];
+// Opening and closing tag that we have to match
+
+const matchTag = "<parse";
+const matchEndTag = "</parse>";
 
 // Array of starting <parse> tag indexes
-let parseStack = []
+const parseStack = [];
 // Array of closing </parse> tag indexes
-let endParseStack = []
+const endParseStack = [];
 
 // Array of not valid string intervals
-let notValid = []
+const notValid = [];
 // Array of intervals where tags are not valid
-let notValidIndexes = []
+const notValidIndexes = [];
 
-// source file path
-const file = './files/test.html';
-// source file string
-const text = openHtmlFile(file)
-//copy of the file string
-let text_copy = text.slice()
+// PARSER function =====================================================================================================
+function parser(filename) {
 
-// OpenHtmlFile reads the html file and returns a string
-function openHtmlFile(file) {
+    // Source file string
+    const text = openHtmlFile(filename);
+    // Copy of the file string
+    let text_copy = text.slice();
+
+    for (let i = 0; i < notAllowed.length; i++) {
+        findNotAllowed(text_copy, notAllowed[i], notAllowedEnd[i]);
+    }
+
+    findNotAllowedIndexes(text, notValid);
+    findParseTag(text, matchTag);
+    findCloseTag(text, matchEndTag);
+
+    // const validIndexes = validateParseIndexes(indexes = parseStack, intervals = notValidIndexes) ???
+    const validIndexes = validateParseIndexes(parseStack, notValidIndexes);
+    // console.log("Valid parse tags starting indexes: ", validIndexes)
+
+    // const validClosingIndexes = validateParseIndexes(indexes = endParseStack, intervals = notValidIndexes) ???
+    const validClosingIndexes = validateParseIndexes(endParseStack, notValidIndexes);
+    // console.log("Valid parse tags closing indexes: ", validClosingIndexes)
+
+    const pairParse = matchPairs(validIndexes, validClosingIndexes);
+    // console.log(pairParse)
+
+    const validParsePairs = removeChildren(pairParse);
+    // console.log("Valid parent parse tags intervals:", validParsePairs)
+
+    const parseStrings = parseStringMaker(text, validParsePairs);
+    // console.log(parseStrings)
+
+    const parseProps = propExtract.extractParseProp(parseStrings);
+    // console.log("Parse properties: ", parseProps)
+
+    const results = propExtract.resultMaker(parseProps, parseStrings, validParsePairs);
+
+    console.dir(results, {
+        depth: 4
+    });
+}
+//======================================================================================================================
+
+
+
+
+// OpenHtmlFile reads the html file and returns a string ===============================================================
+function openHtmlFile(filename) {
     try {
-        const content = fs.readFileSync(file, {
-            encoding: "utf-8"
-        });
-
-        // console.log(content.toString());
-        return content
+        return fs.readFileSync(filename, {encoding: "utf-8"});
     } catch (error) {
-        console.error(`Got an error trying to read the 
-    file: ${error.message}`);
+        console.error(`Got an error trying to read the file: ${error.message}`);
     }
 }
+//======================================================================================================================
 
-// This function find not allowed tags and populate the notValid array with the complete tag substring
+
+// This function find not allowed tags and populate the notValid array with the complete tag substring =================
 function findNotAllowed(text, startingTag, endingTag) {
+
     // counters for delimiting start and end of the not valid interval
-    let start = 0
-    let end = 0
+    let start = 0;
+    let end = 0;
+
     if (text.indexOf(startingTag) !== -1) {
-        start = text.indexOf(startingTag)
+        start = text.indexOf(startingTag);
+
         if (text.indexOf(endingTag, start) !== -1) {
-            end = text.indexOf(endingTag, start) + endingTag.length
-            let toRemove = text.substring(start, end)
-            notValid.push(toRemove)
+            end = text.indexOf(endingTag, start) + endingTag.length;
+
+            const toRemove = text.substring(start, end);
+            notValid.push(toRemove);
             // console.log(toRemove)
-            text = text.replace(toRemove, "")
+            text = text.replace(toRemove, "");
+
             return findNotAllowed(text, startingTag, endingTag)
+
         } else {
-            throw Error("closing tag not found")
+            throw Error("closing tag not found");
         }
     }
-    return false
+    return false;
 }
+//======================================================================================================================
 
-// This function finds the invalid substring in the original text and will populate the notValidIndexes
-// with the invalid intervals of the original text
+
+/* This function finds the invalid substring in the original text and will populate the notValidIndexes ================
+   with the invalid intervals of the original text */
 function findNotAllowedIndexes(text, notValid) {
     notValid.forEach(substr => {
         // console.log(substr)
-        let start = text.indexOf(substr)
-        let end = start + substr.length
-        notValidIndexes.push([start, end])
+        const start = text.indexOf(substr);
+        let end = start + substr.length;
+        notValidIndexes.push([start, end]);
     });
 }
+//======================================================================================================================
 
-// This function will find valid parse tags
+
+// This function will find valid parse tags ============================================================================
 function findParseTag(text, matchTag, start = 0) {
+
     if (text.indexOf(matchTag, start) !== -1) {
-        let startIndex = text.indexOf(matchTag, start)
-        parseStack.push(startIndex)
-        start = startIndex + matchTag.length
-        return findParseTag(text, matchTag, start)
-    }
-    return false
-}
+        const startIndex = text.indexOf(matchTag, start);
+        parseStack.push(startIndex);
+        start = startIndex + matchTag.length;
 
-// This function will find valid closing parse tags
+        return findParseTag(text, matchTag, start);
+    }
+    return false;
+}
+//======================================================================================================================
+
+
+// This function will find valid closing parse tags ====================================================================
 function findCloseTag(text, matchEndTag, start = 0) {
-    if (text.indexOf(matchEndTag, start) !== -1) {
-        let endIndex = text.indexOf(matchEndTag, start) + matchEndTag.length
-        endParseStack.push(endIndex)
-        return findCloseTag(text, matchEndTag, start = endIndex)
-    }
-    return false
-}
 
-// Function to check if parse tags indexes are included in not allowed intervals
-// will return only valid indexes  
+    if (text.indexOf(matchEndTag, start) !== -1) {
+        const endIndex = text.indexOf(matchEndTag, start) + matchEndTag.length;
+        endParseStack.push(endIndex);
+        return findCloseTag(text, matchEndTag, endIndex);
+    }
+    return false;
+}
+//======================================================================================================================
+
+
+/* Function to check if parse tags indexes are included in not allowed intervals =======================================
+   will return only valid indexes */
 function validateParseIndexes(indexes, intervals) {
-    let notValids = []
+
+    const notValids = [];
+
     for (let i in indexes) {
         for (let y in intervals) {
-            if (indexes[i] > intervals[y][0] && indexes[i] < intervals[y][1]) {
-                notValids.push(indexes[i])
-            }
+            if (indexes[i] > intervals[y][0] && indexes[i] < intervals[y][1]) notValids.push(indexes[i])
         }
     }
-    return indexes.filter(el => !notValids.includes(el))
-}
 
-// Function to match parse tags togheter
-// this function return a list of couple of numbers, the first number is the index of the start of the tag and the second is the index of the end of the tag
+    return indexes.filter(el => !notValids.includes(el));
+}
+//======================================================================================================================
+
+
+/* Function to match parse tags together. ==============================================================================
+   This function return a list of couple of numbers,
+   - the first number is the index of the start of the tag and
+   - the second is the index of the end of the tag */
 function matchPairs(validIndexes, validClosingIndexes) {
-    const pairParse = []
+
+    const pairParse = [];
+
     for (let cdx in validClosingIndexes) {
         for (let opx in validIndexes) {
             //console.log(validIndexes)
@@ -121,62 +184,47 @@ function matchPairs(validIndexes, validClosingIndexes) {
             //console.log(validClosingIndexes[cdx])
             //console.log(validIndexes[opx])
             if (validClosingIndexes[cdx] > validIndexes[opx]) {
-                pairParse[cdx] = [validIndexes[opx], validClosingIndexes[cdx]]
+                pairParse[cdx] = [validIndexes[opx], validClosingIndexes[cdx]];
             } else {
-                validIndexes = validIndexes.filter(el => el !== validIndexes[opx - 1])
-                break
+                validIndexes = validIndexes.filter(el => el !== validIndexes[opx - 1]);
+                break;
             }
         }
     }
-    return pairParse
-}
 
-// This function gets rid of parse tags nested inside other parse tags
+    return pairParse;
+}
+//======================================================================================================================
+
+
+// This function gets rid of parse tags nested inside other parse tags =================================================
 function removeChildren(pairParse) {
-    const validParsePairs = []
+
+    const validParsePairs = [];
+
     for (let i = 0; i < pairParse.length - 1; i++) {
         // console.log(pairParse[i + 1])
-        if (pairParse[i][0] < pairParse[i + 1][0]) {
-            validParsePairs.push(pairParse[i])
-        }
+        if (pairParse[i][0] < pairParse[i + 1][0]) validParsePairs.push(pairParse[i])
     }
-    validParsePairs.push(pairParse[pairParse.length - 1])
-    return validParsePairs
-}
 
-// this function will search for parsetag intervals in text and return an array of strings of parse tags
+    validParsePairs.push(pairParse[pairParse.length - 1]);
+    return validParsePairs;
+}
+//======================================================================================================================
+
+
+// This function will search for parse tag intervals in text and return an array of strings of parse tags ==============
 function parseStringMaker(text, parseIntervals) {
-    let parseStrings = []
-    parseIntervals.forEach(elem => parseStrings.push(text.substring(elem[0], elem[1])))
-    return parseStrings
-}
 
-function main() {
-    for (let i = 0; i < notAllowed.length; i++) {
-        findNotAllowed(text_copy, notAllowed[i], notAllowedEnd[i])
-    }
-    findNotAllowedIndexes(text, notValid)
-    findParseTag(text, matchTag)
-    findCloseTag(text, matchEndTag)
-    let validIndexes = validateParseIndexes(indexes = parseStack, intervals = notValidIndexes)
-    // console.log("Valid parse tags starting indexes: ", validIndexes)
-    let validClosingIndexes = validateParseIndexes(indexes = endParseStack, intervals = notValidIndexes)
-    // console.log("Valid parse tags closing indexes: ", validClosingIndexes)
-    const pairParse = matchPairs(validIndexes, validClosingIndexes)
-    // console.log(pairParse)
-    const validParsePairs = removeChildren(pairParse)
-    // console.log("Valid parent parse tags intervals:", validParsePairs)
-    const parseStrings = parseStringMaker(text, validParsePairs)
-    // console.log(parseStrings)
-    const parseProps = extractParseProp(parseStrings)
-    // console.log("Parse properties: ", parseProps)
-    const results = resultMaker(parseProps, parseStrings, validParsePairs)
-    console.dir(results, {
-        depth: 4
-    })
-}
+    const parseStrings = [];
 
-main()
+    parseIntervals.forEach(elem => parseStrings.push(text.substring(elem[0], elem[1])));
+
+    return parseStrings;
+}
+//======================================================================================================================
+
+
 
 // console.log("Invalid intervals where we shouldn't search: ", notValidIndexes)
 // console.log("All starting parseTag indexes: ", parseStack)
@@ -186,3 +234,8 @@ main()
 // TODO: (1) fix removeChildren function to work with multiple children parseTag
 // TODO: (2) currently the code functions only with closing tags of this type </parse> and double quotes for properties. We should fix the rest of the requirements.
 // TODO: (3) refactor code for better readability and debug
+
+
+module.exports = {
+    parser
+};
