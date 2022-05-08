be7551db6efd22655fa7bd523cad7466e035b5b5
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2022-05-08 13:13:10 +0200

    refactor: improved tests findNotAllowed function

--- parser/parser.test.js
+++ parser/parser.test.js
@@ -2,41 +2,33 @@
 const parser = require('./parser')
 
 
-test("Test FindNotAllowed Function" , () => {
+test("Tests FindNotAllowed Function" , () => {
 
     // Style Tag
-    const styleTags = ['<style>', '</style>']
-    const styleNotValid = "<style> .style { background-image: url = '<parse property=\"foo\" </parse>'; } </style>"
-    const styleNotValidTrue = `True${styleNotValid}`
-    const styleNotValidFalse = `${styleNotValid}False`
-    const styleNotValidAdvanced = `True${styleNotValid}False${styleNotValidTrue}FalseTrue${styleNotValidFalse}True`
-    expect(parser.findNotAllowed(styleNotValid, styleTags[0], styleTags[1])).toBe('');
-    expect(parser.findNotAllowed(styleNotValidTrue, styleTags[0], styleTags[1])).toBe('True');
-    expect(parser.findNotAllowed(styleNotValidFalse, styleTags[0], styleTags[1])).toBe('False');
-    expect(parser.findNotAllowed(styleNotValidAdvanced, styleTags[0], styleTags[1]))
-        .toBe('TrueFalseTrueFalseTrueFalseTrue');
-
+    const styleTags = ['<style>', '</style>'];
+    const styleNotValidOne = "<style> .style { background-image: url = '<parse property=\"foo\" </parse>'; } </style>";
     // Script Tag
-    const scriptTags = ['<script>', '</script>']
-    const scriptNotValid = "<script> const url = '<parse property=\"foo\" />' </script>"
-    const scriptNotValidTrue = `True${scriptNotValid}`
-    const scriptNotValidFalse = `${scriptNotValid}False`
-    const scriptNotValidAdvanced = `True${scriptNotValid}False${scriptNotValidTrue}FalseTrue${scriptNotValidFalse}True`
-    expect(parser.findNotAllowed(scriptNotValid, scriptTags[0], scriptTags[1])).toBe('');
-    expect(parser.findNotAllowed(scriptNotValidTrue, scriptTags[0], scriptTags[1])).toBe('True');
-    expect(parser.findNotAllowed(scriptNotValidFalse, scriptTags[0], scriptTags[1])).toBe('False');
-    expect(parser.findNotAllowed(scriptNotValidAdvanced, scriptTags[0], scriptTags[1]))
-        .toBe('TrueFalseTrueFalseTrueFalseTrue');
-
-    // Other Script Tag
+    const scriptTags = ['<script>', '</script>'];
+    const scriptNotValidOne = "<script> const url = '<parse property=\"foo\" />' </script>";
     // const otherScriptNotValid = "<script src='index.js'></script>" // TEST NOT PASSED - Rule HTML ???
-    const otherScriptNotValid = "<script> src='index.js' </script>"
-    const otherScriptNotValidTrue = `True${otherScriptNotValid}`
-    const otherScriptNotValidFalse = `${otherScriptNotValid}False`
-    const otherScriptNotValidAdvanced = `True${otherScriptNotValid}False${scriptNotValidTrue}FalseTrue${scriptNotValidFalse}True`
-    expect(parser.findNotAllowed(otherScriptNotValid, scriptTags[0], scriptTags[1])).toBe('');
-    expect(parser.findNotAllowed(otherScriptNotValidTrue, scriptTags[0], scriptTags[1])).toBe('True');
-    expect(parser.findNotAllowed(otherScriptNotValidFalse, scriptTags[0], scriptTags[1])).toBe('False');
-    expect(parser.findNotAllowed(otherScriptNotValidAdvanced, scriptTags[0], scriptTags[1]))
-        .toBe('TrueFalseTrueFalseTrueFalseTrue');
+    const ScriptNotValidTwo = "<script> src='index.js' </script>";
+    const toTest = [
+        [styleNotValidOne, styleTags],
+        [scriptNotValidOne, scriptTags],
+        [ScriptNotValidTwo, scriptTags],
+    ];
+    // Tests
+    toTest.map(elem => {
+        const notValid = elem[0];
+        const startTag = elem[1][0];
+        const endTag = elem[1][1];
+        const notValidTrue = `True${notValid}`;
+        const notValidFalse = `${notValid}False`;
+        const notValidAdvanced = `True${notValid}False${notValidTrue}FalseTrue${notValidFalse}True`;
+        expect(parser.findNotAllowed(notValid, startTag, endTag)).toBe('');
+        expect(parser.findNotAllowed(notValidTrue, startTag, endTag)).toBe('True');
+        expect(parser.findNotAllowed(notValidFalse, startTag, endTag)).toBe('False');
+        expect(parser.findNotAllowed(notValidAdvanced, startTag, endTag))
+            .toBe('TrueFalseTrueFalseTrueFalseTrue');
+    })
 });
